f34d381ebaddf8a6b51478b02bf10b38a2e217bc
postwo <codo7717@naver.com>
2024-05-04 21:08:56 +0900

    main footer output

---
+++ src/main/java/com/example/phproject/direction/controller/FormController.java
@@ -0,0 +1,32 @@
+package com.example.phproject.direction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+public class FormController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+
+    @PostMapping("/search")
+    public ModelAndView postDirection(@ModelAttribute InputDto inputDto)  {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("output");
+        modelAndView.addObject("outputFormList",
+                pharmacyRecommendationService.recommendPharmacyList(inputDto.getAddress()));
+
+        return modelAndView;
+    }
+
+
+
+}
